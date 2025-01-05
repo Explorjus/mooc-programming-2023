4178ba8afcdbbbb46986bf767d34f7cfe30f1ec5
Explorjus <ja.kubson022@gmail.com>
2025-01-05 22:08:28 +0100

    nowe

--- part04-14_addition_and_removal/src/addition_and_removal.py
+++ part04-14_addition_and_removal/src/addition_and_removal.py
@@ -1,1 +1,20 @@
 # Write your solution here
+lista = []
+num = 1
+print("The list is now", lista)
+while True:
+    wej = input("a(d)d, (r)emove or e(x)it: ")
+
+    if wej == "d":
+        lista.append(num)
+        print("The list is now", lista)
+        num += 1
+    
+    if wej == "r" and len(lista) > 0:
+        lista.pop()
+        print("The list is now", lista)
+        num -= 1
+
+    if wej == "x":
+        print("Bye!")
+        break
--- part04-15_same_word_twice/src/same_word_twice.py
+++ part04-15_same_word_twice/src/same_word_twice.py
@@ -1,1 +1,13 @@
 # Write your solution here
+lista = []
+liczba_wpisanych_slow = 0
+
+while True:
+    word = input("Word: ")
+    lista.append(word)
+    liczba_wpisanych_slow += 1
+    
+    if word in lista and lista.count(word) > 1:
+        print("You typed in", liczba_wpisanych_slow - 1, "different words")
+        break
+
